08ace819fb13dc18eb34dce91a2e0e0336a5b154
Mariana Lopez <mariana.lopez.it@gmail.com>
2022-03-21 20:13:33 -0300

    Add files via upload

---
+++ Unidad 2/unidad2.js
@@ -0,0 +1,19 @@
+//unidad 2
+let cantidadDeVentanasEnMiCasa = 15;
+console.log(`Mi casa tiene ${cantidadDeVentanasEnMiCasa} ventanas`);
+let soyHumano = true;
+console.log(`¿Soy un humano? ${soyHumano}`);
+let miGustoDePizzaPreferido = "Jamon y queso";
+console.log(`Mi pizza favorita es la de ${miGustoDePizzaPreferido}`);
+
+let CambioDeVariable = false;
+console.log(CambioDeVariable);
+
+CambioDeVariable = "Hola";
+console.log(CambioDeVariable);
+
+CambioDeVariable = 0;
+console.log(CambioDeVariable);
+
+CambioDeVariable = "Minions";
+console.log(CambioDeVariable);
