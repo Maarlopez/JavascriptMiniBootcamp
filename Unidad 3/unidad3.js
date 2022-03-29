7cfefc8899a221506751f22871490018ced9ddd8
Maarlopez <mariana.lopez.it@gmail.com>
2022-03-28 23:31:24 -0300

    Unidad 3 parte 2

--- Unidad 3/unidad3.js
+++ Unidad 3/unidad3.js
@@ -122,3 +122,107 @@ console.log(estoyAprobado);
 
 let esHoraDeAlmorzar = (22<=12);
 console.log(esHoraDeAlmorzar);
+
+/********************************************************************************* */
+
+// TP 3 - 2
+
+// 1 - Definición de Algoritmo (dar un ejemplo)
+
+/* Un algoritmo es un conjunto ordenado y finito de operaciones que deben seguirse para resolver un problema.
+Deseas saber el número telefónico de una persona, entonces el algoritmo
+para resolver este problema sería:
+1. Primero buscar la guía telefónica.
+2. Luego abrirla en la mitad.
+3. Luego ver en qué mitad está el nombre (si el nombre empieza con "b"
+entonces está en la primera mitad)
+4. Luego se toma esta mitad y se vuelve a partir y vuelves a ver en cual nueva
+mitad está el nombre, y así se va repitiendo (eso es un bucle) hasta que
+encuentras la página.
+5. Luego buscas en la página de arriba hacia abajo el nombre.
+6. Luego que lo encuentras vas a columna del número.
+7. Lo lees.
+8. Y cierras la guía.*/
+
+// 2 - Que es Pseudocodigo (dar un ejemplo)
+
+/*El pseudocódigo es una forma de escribir los pasos que va a ejecutar un programa de la 
+forma más cercana al lenguaje de programación que se va a realizar.*/
+
+// 3 - Realizar los siguientes problemas:
+
+// a)Realizar un Programa en JavaScript que resuelva los siguientes problemas (los valores deben ser asignados 
+//por ustedes):
+    //superficie de un rectángulo
+    //superficie de un triangulo
+    //superficie de un circulo
+
+let baseRectangulo = 2;
+let alturaRectangulo = 3;
+let superficieRectangulo = baseRectangulo * alturaRectangulo;
+
+console.log(superficieRectangulo);
+
+/**/ 
+
+let baseTriangulo = 5;
+let alturaTriangulo = 3;
+let superficieTriangulo = (baseTriangulo*alturaTriangulo)/2;
+
+console.log(superficieTriangulo);
+
+/**/ 
+
+const pi = 3.14;
+var radioCirculo = 8;
+var superficieCirculo = (pi * (radioCirculo * radioCirculo))
+
+console.log(superficieCirculo);
+
+
+// b) Crear 3  variables numéricas, calcular e imprimir producto, suma y el promedio. Mostrar los resultados.
+
+let num1 = 2;
+let num2 = 4;
+let num3 = 6;
+
+let producto = num1 * num2 * num3;
+let suma = num1 + num2 + num3;
+let promedio = suma / 3;
+
+console.log(producto);
+console.log(suma);
+console.log(promedio);
+
+// c) Una tienda ofrece un descuento del 15% sobre el total de cada compra y un cliente 
+//desea saber cuánto deberá pagar finalmente por su compra. Deberán crear 5 artículos los cuales 
+//tendrán sus nombres y valores, luego mostrar el descuento aplicado mas el total final
+
+let tazas = 7;
+let agendas = 2;
+let lapices = 8;
+let lapiceras = 2;
+let notasAdhesivas = 6;
+let totalProductos = tazas + agendas + lapices + lapiceras + notasAdhesivas;
+let descuento = ((totalProductos) * 0.15);
+console.log("El descuento aplicado es: $" + descuento);
+let totalConElDescuento = totalProductos - descuento;
+console.log("El total final con el descuento es: $" + totalConElDescuento);
+
+
+// d) Dada una cantidad en pesos (el valor lo deben asignar ustedes), obtener la equivalencia en dólares, 
+//asumiendo que la unidad cambiaría es un dato desconocido.
+
+let cantidadPesos = 800;
+let valorDolar = 200;
+let equivalenciaEnDolares = cantidadPesos/valorDolar;
+
+console.log(equivalenciaEnDolares);
+
+// e) Un  cliente realizo una compra y necesita saber el importe del IVA de la misma. 
+//Realizar un algoritmo que dado el importe de una factura calcular el valor correspondiente al IVA.
+
+let compra = 850;
+const tasaDeIva = 0.21;
+let iva = compra * tasaDeIva;
+console.log("El IVA es de: ", iva);
